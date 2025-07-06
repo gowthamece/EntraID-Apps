8b4a37453909816aa336d060da3440565db38982
Gowtham <gowthamk91@outlook.com>
2025-07-06 09:29:34 -0400

    Added External ID Project

---
+++ External-EntraID-WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace External_EntraID_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
